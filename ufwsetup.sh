61b3a2f38fa74da1163741a41eb1d688bd241637
Nuno <75589883+nuno-src@users.noreply.github.com>
2022-08-06 23:32:06 +0100

    Create ufwsetup.sh

---
+++ ufwsetup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# intall ufw
+sudo apt update
+sudo apt install ufw
+
+# ufw configs:
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw enable
+
+sudo systemctl status ufw
+
+sudo ufw status
+sudo ufw status numbered
